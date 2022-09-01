2642e4d089121dc8c79274e2c698d7ce81271082
stefanpython <stefanhtml@yahoo.com>
2022-09-01 14:41:38 +0300

    Modify height & width of div container

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ let gridSize = 16;
 
 for (let i = 0; i < gridSize ** 2; i++) {
     const div = document.createElement('div');
-    div.style.cssText = "border: 1px solid black; height: 25px; width: 25px";
+    div.style.cssText = "border: 1px solid black; height: 35px; width: 35px";
     div.classList = 'square'
     container.appendChild(div);
 }
