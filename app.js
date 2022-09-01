52086466d041dbdd6dda238169c4b944face1bfd
stefanpython <stefanhtml@yahoo.com>
2022-09-01 17:47:34 +0300

    Add click event to button

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
+const button = document.querySelector("button");
+button.addEventListener("click", createGrid);
+
 function createGrid() {
     
     let container = document.querySelector(".container");
-    let numOfRows = 16; //prompt("Enter number of rows <= 100.");
+    let numOfRows = prompt("Enter number of rows <= 100.");
+
+    while (numOfRows > 100) {
+        numOfRows = prompt("Enter number of rows <= 100.");
+    }
     
     // Set value of the variable --columns-row in CSS with the value of the variable numOfRows
     document.documentElement.style.setProperty("--columns-row", numOfRows);
@@ -9,10 +16,11 @@ function createGrid() {
     for (let i =  0; i < numOfRows ** 2; i++) {
       let div = document.createElement("div");
       container.appendChild(div);
+      // Add hover effect when passing mouse over squares
       div.addEventListener("mouseenter", () => {
         div.style.background = '#009999';
       })
     }
   }
   
-  createGrid();
+
