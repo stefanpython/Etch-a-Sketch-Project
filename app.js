2c4ac5e8ec41ae7848e60ac460ce4089af530648
stefanpython <stefanhtml@yahoo.com>
2022-09-01 17:35:12 +0300

    Add hover when passing mouse over divs

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ function createGrid() {
     let container = document.querySelector(".container");
     let numOfRows = 16; //prompt("Enter number of rows <= 100.");
     
-    // Replace the --columns-row value in css with numOfRows value
+    // Set value of the variable --columns-row in CSS with the value of the variable numOfRows
     document.documentElement.style.setProperty("--columns-row", numOfRows);
 
     for (let i =  0; i < numOfRows ** 2; i++) {
       let div = document.createElement("div");
       container.appendChild(div);
-    
+      div.addEventListener("mouseenter", () => {
+        div.style.background = '#009999';
+      })
     }
   }
   
